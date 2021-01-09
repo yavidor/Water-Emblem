2b9cfd83cc0f8b147440dc1a0c5af0bc53c9edca
yavidor <yonatan@avidor.org>
2021-01-09 18:41:35 +0200

    Honestly I don't really know how to continue. I have no idea what the next step should be

---
+++ testing/testing/Weapon.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testing
+{
+    class Weapon
+    {
+        int mt, rng, wt;
+        float jot, crt;
+    }
+}
